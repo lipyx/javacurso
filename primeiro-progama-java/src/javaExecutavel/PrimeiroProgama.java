66b0e82958211b882d61afd3d61a1f53c04cbd5a
lipy <lipy@DESKTOP-7UCV0DS>
2022-05-19 15:18:09 -0300

    test

--- primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
+++ primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
@@ -1,10 +1,10 @@
 package javaExecutavel;
 
 public class PrimeiroProgama {
-	/*testando commit*/
+	/*testando commit, nao to conseguindo*/
 	public static void main(String[] args) {
 		
-		System.out.println("Olá Mundo! primeiro progama em Java");
+		System.out.println("Olá Mundo! primeiro progama em Java :D oi");
 		
 	}
 	
