0f7085c51d9f7afc3bddbe91c483d7d7fa40641e
lipy <lipy@DESKTOP-7UCV0DS>
2022-05-19 14:50:49 -0300

    Criando projeto java

---
+++ primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
@@ -0,0 +1,12 @@
+package javaExecutavel;
+
+public class PrimeiroProgama {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo! primeiro progama em Java");
+		
+	}
+	
+
+}
