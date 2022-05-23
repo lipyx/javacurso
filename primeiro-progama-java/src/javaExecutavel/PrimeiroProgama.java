2d7b246a8ad431d3c9f1e7f1de0f8836540564c1
lipy <lipy@DESKTOP-7UCV0DS>
2022-05-23 11:45:00 -0300

    Revert "Criado váriavel data de nascimento"
    This reverts commit 48044e86daf752c4fba8617fc5c7968def5573a5.

--- primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
+++ primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
@@ -1,23 +1,11 @@
 package javaExecutavel;
 
-import java.sql.Date;
-
 public class PrimeiroProgama {
-	private static Date nascimento;
-
 	/*testando commit, se alterar o projeto no github ja era.*/
 	public static void main(String[] args) {
 		
 		System.out.println("Olá Mundo! primeiro progama em Java :D oi");
 		
-	}
-
-	public static Date getNascimento() {
-		return nascimento;
-	}
-
-	public static void setNascimento(Date nascimento) {
-		PrimeiroProgama.nascimento = nascimento;
 	}
 	
 
