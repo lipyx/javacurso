7ecd11478d95bdd2e69f9fb0f3b01a3430d98056
lipy <lipy@DESKTOP-7UCV0DS>
2022-05-24 12:44:40 -0300

    Mircro validações

--- primeiro-progama-java2/src/javaExecutavel/PrimeiroProgama2.java
+++ primeiro-progama-java2/src/javaExecutavel/PrimeiroProgama2.java
@@ -13,14 +13,14 @@ public class PrimeiroProgama2 {
 		System.out.println("Olá Mundo! primeiro progama em Java :D oi");
 		/* Essas váriaveis são locais, portanto acessiveis somente a esse metodo */
 
-		float nota1 = 50;
-		float nota2 = 75;
+		float nota1 = 20;
+		float nota2 = 15;
 		float nota3 = 65;
-		float nota4 = 59;
+		float nota4 = 9;
 		float mediaNota = 0;
 
 		mediaNota = (nota1 + nota2 + nota3 + nota4) / 4;
-
+/*
 		String name = "lipy";
 
 		if (mediaNota >= 70 && name.equals("lipy")) {
@@ -30,8 +30,12 @@ public class PrimeiroProgama2 {
 		} else {
 			System.out.println("Você foi reprovado: " + mediaNota);
 
-		}
-
+		}*/
+		
+		/*Operadores ternários de micro validções*/
+		
+		String saidaResultado = mediaNota >= 70 ? "aluno aprovado" : (mediaNota >= 40 && mediaNota <=69) ? "aluno em recuperação" : "reprovado";
+		System.out.println(saidaResultado);
 	}
 
 }
