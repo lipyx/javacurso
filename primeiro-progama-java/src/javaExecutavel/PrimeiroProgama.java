56443d79a4f20e6f5a59b71e783ee50f0a589c94
lipy <lipy@DESKTOP-7UCV0DS>
2022-05-24 12:10:39 -0300

    uso das operações logicas em Java.

--- primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
+++ primeiro-progama-java/src/javaExecutavel/PrimeiroProgama.java
@@ -1,11 +1,14 @@
 package javaExecutavel;
 
 public class PrimeiroProgama {
+	/*Essa variavel é do tipo Global é acessivel a todos e seu valor é compartilhado*/
+	static int maiorIdadeGlobal = 30;
+	
 	/*testando commit, se alterar o projeto no github ja era.*/
 	public static void main(String[] args) {
 		
 		System.out.println("Olá Mundo! primeiro progama em Java :D oi");
-		
+		/*Essas váriaveis são locais, portanto acessiveis somente a esse metodo*/
 		int maiorIdade = 18;
 		
 		int idosoIdade = 60;
@@ -18,7 +21,14 @@ public class PrimeiroProgama {
 		
 		System.out.println("Maior idade é = " + maiorIdade);
 		System.out.println("Para ser idoso sua idade deve ser maior que " + idosoIdade + " anos.");
+		
+		 metodo2();
 	}
+	
+	public static void metodo2() {
+		System.out.println("Valor da váriavel Global é " + maiorIdadeGlobal);
+	}
+	
 	
 
 }
