21899e80ab29d2dbc31a925e877430b5eb96b9ef
lipy <lipy@DESKTOP-7UCV0DS>
2022-05-25 15:20:35 -0300

    relação carros em estoque versos pessoas

--- CarroXClient/src/javaExecutavel/WhileRepeticao.java
+++ CarroXClient/src/javaExecutavel/WhileRepeticao.java
@@ -1,5 +1,7 @@
 package javaExecutavel;
+
 import javax.swing.JOptionPane;
+
 public class WhileRepeticao {
 	/*
 	 * Essa variavel é do tipo Global é acessivel a todos e seu valor é
@@ -12,25 +14,30 @@ public class WhileRepeticao {
 
 		System.out.println("Olá Mundo! primeiro progama em Java :D oi");
 		/* Essas váriaveis são locais, portanto acessiveis somente a esse metodo */
-		
+
 		String carros = JOptionPane.showInputDialog("informe quantos carros no estoque.");
 		String pessoas = JOptionPane.showInputDialog("Informe quantos pessoas querem comprar.");
-		
+
 		double carrosNumero = Double.parseDouble(carros);
 		double pessoasNumero = Double.parseDouble(pessoas);
-		
-		int divisao = (int)  (carrosNumero / pessoasNumero);
-		
+
+		int divisao = (int) (carrosNumero / pessoasNumero);
+		int faltaCarro = (int) (pessoasNumero - carrosNumero);
+
 		double resto = (carrosNumero % pessoasNumero);
-		
-	
-		
+
 		if (resto == 0) {
-			System.out.println("Não sobraram nenhum carro para venda");
-		}else {
-			System.out.println("Sobraram um total de " + resto + " carros");
+			System.out.println("Os carros foram divididos entre " + pessoasNumero + " pessoas e cada uma levou "
+					+ divisao + (" carros para casa."));
+		} else if (pessoasNumero > carrosNumero) {
+			System.out.println(resto + " conseguiram ter seus carro, porem " + faltaCarro + " ficaram sem carros.");
 		}
-		
+
+		else {
+			System.out.println("Os carros foram divididos ente " + pessoasNumero + " pessoas e sobraram um toral de : "
+					+ resto + "carros no estoque.");
+		}
+
 	}
 
 }
