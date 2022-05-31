4add537dab2b1daddc71d3007debb3f43306f5ad
lipy <lipy@DESKTOP-7UCV0DS>
2022-05-31 14:41:46 -0300

    Classes, metodos Getters and Getters e POO

---
+++ primeiro-progama-java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,116 @@
+package cursojava.classes;
+
+/*Essa é a classe/objeto que representa Aluno*/
+public class Aluno {
+	/* Atributos do Aluno */
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
+
+	public Aluno() {/* Cria os dados na memória - padrão Java */
+
+	}
+
+	public Aluno(String nomePadrao) {
+		nome = nomePadrao;
+
+	}
+
+	public Aluno(String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+
+	}
+	/* Metodos Getters e Setters */
+	/* Setter ou SET, recebe ou adiciona dados para os atributos */
+	/* Getters ou GET, é para resgatar ou obter valores dos atributos */
+	public void setNome(String nome) {
+		this.nome = nome;
+		
+	}
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
+	
+	
+}
---
+++ primeiro-progama-java/src/javaExecutavel/ClasseJava.java
@@ -0,0 +1,33 @@
+package javaExecutavel;
+
+import cursojava.classes.Aluno;
+
+public class ClasseJava {
+
+	public static void main(String[] args) {
+		/* new Aluno() é uma instância (criação de um objeto) */
+		/* aluno1 é uma referência para o objeto */
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Felipe");
+		aluno1.setIdade(30);
+		aluno1.setDataNascimento("19/09/1991");
+		aluno1.setRegistroGeral("47.888.888-9");
+		aluno1.setNumeroCpf("123.123.123-58");
+		aluno1.setNomeMae("Isabel");
+		aluno1.setNomePai("Roberto");
+		aluno1.setDataMatricula("15/15/2022");
+		aluno1.setNomeEscola("Java");
+		aluno1.setSerieMatriculado("2");
+		
+		System.out.println("nome é "+ aluno1.getNome());
+		
+		
+		Aluno aluno5 = new Aluno("Roberto"); /* Construtor com 1 parametro (nome) */
+		Aluno aluno6 = new Aluno("José", 53); /* Construtor com 2 parâmetros (nome, idade) */
+		
+		aluno1.setNome("Andrew Felipe Moreno");
+		System.out.println("o nome do aluno é "+ aluno1.getNome());
+	}
+
+	
+}
