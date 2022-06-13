df3efcfe606d30e77b6255cebeefdc075aa97e3d
lipyx <lipyx23@gmail.com>
2022-06-13 11:21:27 -0300

    test dois

--- primeiro-progama-java/src/status/aprovacao/StatusAluno.java
+++ primeiro-progama-java/src/status/aprovacao/StatusAluno.java
@@ -6,6 +6,6 @@ public class StatusAluno {
 	public static String APROVADO = "Aluno aprovado";
 	public static String RECUPERACAO = "Aluno em recuperação";
 	public static String REPROVADO = "Aluno reprovado";
-
+	public static String PENDENCIAS = "Pendencias de documentos";
 }
 
