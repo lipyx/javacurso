0174300ed6e75ef637e0133714e2bf9cc0aee52a
lipyx <lipyx23@gmail.com>
2022-07-05 13:38:52 -0300

    Manipulando 2 thread ao mesmo tempo

--- primeiro-progama-java/src/cursojava/thread/AulaThread.java
+++ primeiro-progama-java/src/cursojava/thread/AulaThread.java
@@ -7,19 +7,38 @@ public class AulaThread {
 	public static void main(String[] args) throws InterruptedException {
 
 		new Thread() {
+
 			// comando que executa o codigo dentro da thread
 			public void run() {
 
 				for (int pos = 0; pos < 10; pos++) {
 					try {
 						Thread.sleep(1000);
-						System.out.println("Exemplo de espera, manipulando as thread.");
+						System.out.println("Exemplo de espera, manipulando as thread.Envio de e-mail");
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 
 					}
+				}
+			}
 
+		}.start();// comando "start" inicia a thread
+
+		/*-------------- DIVISÃO DE THREAD------------*/
+
+		new Thread() {
+
+			// comando que executa o codigo dentro da thread
+			public void run() {
+
+				for (int pos = 0; pos < 10; pos++) {
+					try {
+						Thread.sleep(2000);
+						System.out.println("Exemplo de espera, manipulando as thread.Emissão de nota");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+
+					}
 				}
 			}
 
