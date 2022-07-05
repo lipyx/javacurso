002cf1d3c0876276e38facee8a26e932b67a7b1a
lipyx <lipyx23@gmail.com>
2022-07-05 13:26:19 -0300

    manipulando a thread

--- primeiro-progama-java/src/cursojava/thread/AulaThread.java
+++ primeiro-progama-java/src/cursojava/thread/AulaThread.java
@@ -1,16 +1,31 @@
 package cursojava.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		
-		
-		for (int pos = 0; pos < 10; pos++) {
-			Thread.sleep(1000);
-			System.out.println("Exemplo de espera manipulando as thread.");
-		}
-		
+		new Thread() {
+			// comando que executa o codigo dentro da thread
+			public void run() {
+
+				for (int pos = 0; pos < 10; pos++) {
+					try {
+						Thread.sleep(1000);
+						System.out.println("Exemplo de espera manipulando as thread.");
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+
+					}
+
+				}
+			}
+
+		}.start();// comando "start" inicia a thread
+
+		JOptionPane.showMessageDialog(null, "O progama esta terminando o processo por trás dos sistema");
 	}
 
 }
