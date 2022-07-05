2e991f39c0367ef7467958b04cb3ed23f572b2f2
lipyx <lipyx23@gmail.com>
2022-07-05 13:13:08 -0300

    estudos de thread

---
+++ primeiro-progama-java/src/cursojava/thread/AulaThread.java
@@ -0,0 +1,16 @@
+package cursojava.thread;
+
+public class AulaThread {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		
+		
+		for (int pos = 0; pos < 10; pos++) {
+			Thread.sleep(1000);
+			System.out.println("Exemplo de espera manipulando as thread.");
+		}
+		
+	}
+
+}
