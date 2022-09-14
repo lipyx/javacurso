d58f646d1ae52264b0a8591c830394b87059d2fa
Lipyx <lipyx23@gmail.com>
2022-09-14 15:43:31 -0300

    LocalDate API algumas possibilidades para futuros projetos

--- primeiro-progama-java/src/cursojava/datas/ParcelasMensais.java
+++ primeiro-progama-java/src/cursojava/datas/ParcelasMensais.java
@@ -1,12 +1,41 @@
 package cursojava.datas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
 public class ParcelasMensais {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
+		Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("15/09/2022"); 
 		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dataInicial);
 		
+		for (int parcela = 1; parcela <= 12; parcela++) {
+			
+			calendar.add(Calendar.MONTH, 1);
+			
+			System.out.println("Parcela número " + parcela + " vencimento é em :" + 
+			new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		}
 		
+		//API data java 8, LocalDate, LocalTime e LocalDateTime
+			
+		String dataAtual = LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		System.out.println("data atual " + dataAtual);
+		
+		LocalTime hora = LocalTime.now();
+		System.out.println("hora atual: " + hora);
+	
+		String diaHora = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm.ss"));
+		System.out.println("Dia e hora atual: "+ diaHora);
+
 	}
-
 }
